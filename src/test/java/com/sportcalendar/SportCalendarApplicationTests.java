964e71a92fe925bc4c126b24e6e6dd660cce4c79
Bartłomiej Kotapka <bartek.kotapka32@gmail.com>
2026-03-29 20:19:22 +0200

    Initial commit Sportradar

---
+++ src/test/java/com/sportcalendar/SportCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
